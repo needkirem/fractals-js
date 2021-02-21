b6eb36f619fc83dee5839f02f46eeddcf85dd215
needkirem <needkirem@gmail.com>
2021-02-21 23:04:39 +0500

    Dragon curve added

--- js/base_scripts.js
+++ js/base_scripts.js
@@ -1,21 +1,30 @@
 function on_change_params() {
-    let deep = document.getElementById('deep').value
+    let fractal_types = document.getElementById('fractal-types')
+    let parent = document.getElementById("canvas-container");
+    let range = document.getElementById('deep')
+
+    let deep = range.value
     document.getElementById('deep-title').innerText = '\nDeep is ' + deep
 
     let ctx = get_canvas_context(deep)
-    let parent = document.getElementById("canvas-container");
-
     let y = parent.offsetHeight / 1.5;
     let x = parent.offsetWidth;
 
-    let fractal_types = document.getElementById('fractal-types')
     switch (fractal_types.value) {
         case 'Koch curve':
+            range.max = 10
             koch_curve(ctx, 0, y, x, y, deep)
             break
         case 'Koch showflake':
+            range.max = 10
             y = parent.offsetHeight / 4 * 3
             koch_showflake(ctx, 0, y, x, y, deep)
+            break
+        case 'Dragon curve':
+            range.max = 20
+            y = parent.offsetHeight / 5 * 3;
+            x = parent.offsetWidth / 3;
+            simple_dragon(ctx, x, y, x * 2, y, deep)
             break
     }
 }
@@ -31,7 +40,7 @@ function set_start_params() {
     on_change_params()
 }
 
-function get_canvas_context(deep) {
+function get_canvas_context() {
     let parent = document.getElementById("canvas-container");
     let canvas = document.getElementById("canvas");
 
--- js/fractals.js
+++ js/fractals.js
@@ -51,6 +51,22 @@ function koch_showflake(ctx, x1, y1, x2, y2, deep) {
     koch_curve(ctx, sc_x, sc_y, fs_x, fs_y, deep)
 }
 
+function simple_dragon(ctx, x1, y1, x2, y2, deep) {
+    if (deep > 0) {
+        let sc_x = (x1 + x2) / 2 + (y2 - y1) / 2;
+        let sc_y = (y1 + y2) / 2 - (x2 - x1) / 2;
+
+        simple_dragon(ctx, x1, y1, sc_x, sc_y, deep - 1)
+        simple_dragon(ctx, x2, y2, sc_x, sc_y, deep - 1)
+        return
+    }
+    ctx.strokeStyle = get_random_color()
+    ctx.beginPath()
+    ctx.moveTo(x1, y1)
+    ctx.lineTo(x2, y2)
+    ctx.stroke()
+}
+
 function get_random_color() {
     let r = 255 * Math.random() | 0,
         g = 255 * Math.random() | 0,
